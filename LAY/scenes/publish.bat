a85522ff4cc77c956d66fe33076d07bdd79ed559
Greg Simon <gregsimon@gmail.com>
2019-12-27 12:36:35 -0800

    updating content

--- LAY/scenes/publish.bat
+++ LAY/scenes/publish.bat
@@ -1,28 +1,28 @@
 
 
-copy 54\*.html ..\..\scenes\54\
-copy 54\*.png ..\..\scenes\54\
-copy 54\*.jpg ..\..\scenes\54\
+copy ..\..\scenes\54\*.html 54\
+copy ..\..\scenes\54\*.png 54\
+copy ..\..\scenes\54\*.jpg 54\
 git add 54\*.*
 
-copy 64\*.html ..\..\scenes\64\
-copy 64\*.png ..\..\scenes\64\
-copy 64\*.jpg ..\..\scenes\64\
+copy ..\..\scenes\64\*.html 64\
+copy ..\..\scenes\64\*.png 64\
+copy ..\..\scenes\64\*.jpg 64\
 git add 64\*.*
 
-copy 70\*.html ..\..\scenes\70\
-copy 70\*.png ..\..\scenes\70\
-copy 70\*.jpg ..\..\scenes\70\
+copy ..\..\scenes\70\*.html 70\
+copy ..\..\scenes\70\*.png 70\
+copy ..\..\scenes\70\*.jpg 70\
 git add 70\*.*
 
-copy 100\*.html ..\..\scenes\100\
-copy 100\*.png ..\..\scenes\100\
-copy 100\*.jpg ..\..\scenes\100\
+copy ..\..\scenes\100\*.html 100\
+copy ..\..\scenes\100\*.png 100\
+copy ..\..\scenes\100\*.jpg 100\
 git add 100\*.*
 
-copy 110\*.html ..\..\scenes\110\
-copy 110\*.png ..\..\scenes\110\
-copy 110\*.jpg ..\..\scenes\110\
+copy ..\..\scenes\110\*.html 110\
+copy ..\..\scenes\110\*.png 110\
+copy ..\..\scenes\110\*.jpg 110\
 git add 110\*.*
 
 git commit -a -m "updating content"
