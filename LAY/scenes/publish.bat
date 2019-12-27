712d3d3fb560de6c07d152337db0d079bc482d70
Greg Simon <gregsimon@gmail.com>
2019-12-27 12:33:56 -0800

    removed PSD files and unused scripts

---
+++ LAY/scenes/publish.bat
@@ -0,0 +1,24 @@
+
+
+copy 54\*.html ..\..\scenes\54\
+copy 54\*.png ..\..\scenes\54\
+copy 54\*.jpg ..\..\scenes\54\
+
+copy 64\*.html ..\..\scenes\64\
+copy 64\*.png ..\..\scenes\64\
+copy 64\*.jpg ..\..\scenes\64\
+
+copy 70\*.html ..\..\scenes\70\
+copy 70\*.png ..\..\scenes\70\
+copy 70\*.jpg ..\..\scenes\70\
+
+copy 100\*.html ..\..\scenes\100\
+copy 100\*.png ..\..\scenes\100\
+copy 100\*.jpg ..\..\scenes\100\
+
+copy 110\*.html ..\..\scenes\110\
+copy 110\*.png ..\..\scenes\110\
+copy 110\*.jpg ..\..\scenes\110\
+
+git commit -a -m "updating content"
+git push origin master
