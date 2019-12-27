e3ddd16103b1bb2efe309d9ebb026214adddff24
Greg Simon <gregsimon@gmail.com>
2019-12-27 13:45:55 -0800

    updating content

--- LAY/scenes/publish.bat
+++ LAY/scenes/publish.bat
@@ -1,28 +1,28 @@
 
 
-copy ..\..\scenes\54\*.html 54\
-copy ..\..\scenes\54\*.png 54\
-copy ..\..\scenes\54\*.jpg 54\
+copy ..\..\..\gags\scenes\54\*.html 54\
+copy ..\..\..\gags\scenes\54\*.png 54\
+copy ..\..\..\gags\scenes\54\*.jpg 54\
 git add 54\*.*
 
-copy ..\..\scenes\64\*.html 64\
-copy ..\..\scenes\64\*.png 64\
-copy ..\..\scenes\64\*.jpg 64\
+copy ..\..\..\gags\scenes\64\*.html 64\
+copy ..\..\..\gags\scenes\64\*.png 64\
+copy ..\..\..\gags\scenes\64\*.jpg 64\
 git add 64\*.*
 
-copy ..\..\scenes\70\*.html 70\
-copy ..\..\scenes\70\*.png 70\
-copy ..\..\scenes\70\*.jpg 70\
+copy ..\..\..\gags\scenes\70\*.html 70\
+copy ..\..\..\gags\scenes\70\*.png 70\
+copy ..\..\..\gags\scenes\70\*.jpg 70\
 git add 70\*.*
 
-copy ..\..\scenes\100\*.html 100\
-copy ..\..\scenes\100\*.png 100\
-copy ..\..\scenes\100\*.jpg 100\
+copy ..\..\..\gags\scenes\100\*.html 100\
+copy ..\..\..\gags\scenes\100\*.png 100\
+copy ..\..\..\gags\scenes\100\*.jpg 100\
 git add 100\*.*
 
-copy ..\..\scenes\110\*.html 110\
-copy ..\..\scenes\110\*.png 110\
-copy ..\..\scenes\110\*.jpg 110\
+copy ..\..\..\gags\scenes\110\*.html 110\
+copy ..\..\..\gags\scenes\110\*.png 110\
+copy ..\..\..\gags\scenes\110\*.jpg 110\
 git add 110\*.*
 
 git commit -a -m "updating content"
